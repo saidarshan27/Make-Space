c59a60035496f2616d03091e1a4a00edaf1962d2
rekham13 <darshanrambo25@gmail.com>
2021-11-14 16:52:59 +0530

    bring processCommand function to main program

--- geektrust.js
+++ geektrust.js
@@ -1,4 +1,16 @@
-const { getInput, processCommand } = require('./modules/util');
+const { processBookCommand } = require('./modules/booking');
+const { getInput } = require('./modules/util');
+
+const processCommand = (command) => {
+  if (command.includes('BOOK')) {
+    processBookCommand(command);
+  }
+  // } else if (type === 'VACANCY') {
+  //   processVacancyCommand(command);
+  // } else {
+  //   throw new Error('INCORRECT_INPUT');
+  // }
+};
 
 const main = async () => {
   const inputFilePath = process.argv[2];
